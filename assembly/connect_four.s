e1afcac4a1298f30849885658d53ec744099b25f
Elder Massahiro Yoshida <elder.yoshida@gmail.com>
2014-04-19 16:27:00 -0400

    integrated tiling imaging and background imaging. Changed mif

--- assembly/connect_four.s
+++ assembly/connect_four.s
@@ -1,5 +1,8 @@
 .text
-load_background_0:
+
+
+
+load_background_init:
 	add $r24, $r0, $r0 # $r24= x = 0
 	add $r25, $r0, $r0 # $r25 = y = 0
 	addi $r28, $r0, 2000 # $r28 = memaddress = 2000
@@ -7,7 +10,7 @@ load_background_0:
 	addi $r23, $r0, 160 #width
 	addi $r22, $r0, 22800 #size
 
-load_background_1:
+load_image_1:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
@@ -30,11 +33,11 @@ load_background_1:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_2 #x!=width=160   else...
+	bne  $r24, $r23, load_image_2 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
-load_background_2:
+load_image_2:
 	or $r27, $r26, $r30 # r27 = color 3bit 
 	sra $r27, $r27, 3  #shift 3
 	lw $r30, 3002($r0)   # load mask 2 beforehand
@@ -48,12 +51,12 @@ load_background_2:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_3 #x!=width=160   else...
+	bne  $r24, $r23, load_image_3 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
 
-load_background_3:
+load_image_3:
 	or $r27, $r26, $r30 # r27 = color 3bit 
 	sra $r27, $r27, 6  #shift 6
 	lw $r30, 3003($r0)   # load mask 3 beforehand
@@ -67,11 +70,11 @@ load_background_3:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_6 #x!=width=160   else...
+	bne  $r24, $r23, load_image_6 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
-load_background_4:
+load_image_4:
 	or $r27, $r26, $r30 # r27 = color 3bit 
 	sra $r27, $r27, 9  #shift 9
 	lw $r30, 3004($r0)   # load mask 4 beforehand
@@ -85,12 +88,12 @@ load_background_4:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_5 #x!=width=160   else...
+	bne  $r24, $r23, load_image_5 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
 
-load_background_5:
+load_image_5:
 	or $r27, $r26, $r30 # r27 = color 3bit 
 	sra $r27, $r27, 12  #shift 12
 	lw $r30, 3005($r0)   # load mask 5 beforehand
@@ -104,11 +107,11 @@ load_background_5:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_6 #x!=width=160   else...
+	bne  $r24, $r23, load_image_6 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
-load_background_6:
+load_image_6:
 	or $r27, $r26, $r30 # r27 = color 3bit 
 	sra $r27, $r27, 15  #shift 15
 	lw $r30, 3006($r0)   # load mask 6 beforehand
@@ -122,11 +125,11 @@ load_background_6:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_7 #x!=width=160   else...
+	bne  $r24, $r23, load_image_7 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
-load_background_7:
+load_image_7:
 	or $r27, $r26, $r30 # r27 = color 3bit 
 	sra $r27, $r27, 18  #shift 18
 	lw $r30, 3007($r0)   # load mask 7 beforehand
@@ -140,11 +143,11 @@ load_background_7:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_8 #x!=width=160   else...
+	bne  $r24, $r23, load_image_8 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
-load_background_8:
+load_image_8:
 	or $r27, $r26, $r30 # r27 = color 3bit 
 	sra $r27, $r27, 21  #shift 21
 	lw $r30, 3008($r0)   # load mask 8 beforehand
@@ -158,12 +161,12 @@ load_background_8:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_9 #x!=width=160   else...
+	bne  $r24, $r23, load_image_9 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
 
-load_background_9:
+load_image_9:
 	or $r27, $r26, $r30 # r27 = color 3bit 
 	sra $r27, $r27, 24  #shift 24
 	lw $r30, 3009($r0)   # load mask 9 beforehand
@@ -177,12 +180,12 @@ load_background_9:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_10 #x!=width=160   else...
+	bne  $r24, $r23, load_image_10 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
 
-load_background_10:
+load_image_10:
 	or $r27, $r26, $r30 # r27 = color 3bit 
 	sra $r27, $r27, 27  #shift 27
 	add $r0, $r0, $r0 #nop before output.
@@ -196,14 +199,17 @@ load_background_10:
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
 	add $r0, $r0, $r0 #nop before output.
-	bne  $r24, $r23, load_background_11 #x!=width=160   else...
+	bne  $r24, $r23, load_image_11 #x!=width=160   else...
 	add $r24, $r0, $r0 # $r24= x = 0
 	addi $r25, $r25, 1 # y++
 
-load_background_11:
-	bne  $r29, $r22, load_background_1
+load_image_11:
+	bne  $r29, $r22, load_image_1
 
-load_background_end:
-	add $r0, $r0, $r0 #nop before output.
+load_tile_init:
+	add $r0, $r0, $r0
+
+
+
 
 .data
